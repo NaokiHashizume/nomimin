827902901f40720d2cfeb2a37dae50a4df5be386
NaokiHashizume <naoki.a.hashizume@gmail.com>
2026-03-01 05:02:22 +0900

    飲み会アプリの初期プロジェクト作成

---
+++ nomimin/nomimin/nomiminApp.swift
@@ -0,0 +1,17 @@
+//
+//  nomiminApp.swift
+//  nomimin
+//
+//  Created by Hashizume, Naoki | Hassy | RTS on 2026/03/01.
+//
+
+import SwiftUI
+
+@main
+struct nomiminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
